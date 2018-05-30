da8ba53ec91982bcf2e5ff718fd921121a6646a6
yellowb <122517167@qq.com>
2018-05-30 17:33:24 +0800

    leetcode No2 Add Two Numbers

---
+++ leetcode-problems/src/main/java/medium/No_0002_Add_Two_Numbers.java
@@ -0,0 +1,37 @@
+package medium;
+
+public class No_0002_Add_Two_Numbers {
+
+    static public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = null;
+
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        }
+
+
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+    }
+}
